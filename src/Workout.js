a4fc0096d9fecf03f2566c7d53bec973c4b46480
Priyank Thakkar <priyankthakkar@gmail.com>
2020-05-26 10:34:29 +1000

    Change Log: 1. Updating Workout.js to use attributeHelper. Changes By: Priyank Thakkar

--- src/Workout.js
+++ src/Workout.js
@@ -3,7 +3,7 @@ import { getDataTestAttribute } from "./helpers/attributeHelper";
 
 function Workout({ name, target, group }) {
   return (
-    <div data-test-id="workout-container">
+    <div {...getDataTestAttribute("workout-container")}>
       <h4 data-test-id="workout-name">Name: {name}</h4>
       <h6 data-test-id="workout-target">Target: {target}</h6>
       <h6 data-test-id="workout-group">Group: {group}</h6>
